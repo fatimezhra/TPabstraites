ac4c0455e2dc9af969a2e18b411af2eb6a43e3d8
fatimezhra <ton.email@exemple.com>
2025-12-15 15:39:21 +0100

    Initial commit - TP abstraites

---
+++ src/com/exemple/FileItem.java
@@ -0,0 +1,14 @@
+package com.exemple;
+public class FileItem extends FsItem {
+    private long size; 
+
+    public FileItem(String name, long size) {
+        super(name);
+        this.size = size;
+    }
+
+    @Override
+    public long getSize() {
+        return size;
+    }
+}
